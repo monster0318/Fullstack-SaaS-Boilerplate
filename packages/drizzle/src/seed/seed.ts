0c75d844d23425e57674e7ae23e0f70329ea53cd
Alan <alan345@gmail.com>
2025-03-20 10:30:45 -0700

    better seed

--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -2,8 +2,7 @@ import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { drizzleOrm } from "../packageDrizzle"
 const { eq } = drizzleOrm
-import { userTable, userCredentialTable } from "../db/schema"
-// import { userTable, deviceTable, userCredentialTable } from "../db/schema"
+import { userTable, userCredentialTable, deviceTable } from "../db/schema"
 import * as schema from "../db/schema"
 import dotenv from "dotenv"
 dotenv.config({ path: "../../server.env" })
@@ -12,37 +11,32 @@ const db = drizzle(databaseUrl, { schema })
 
 const main = async () => {
   console.log(`Seeding ${databaseUrl}...`)
-  // const db = drizzle(databaseUrl)
-  // await db.delete(userCredentialTable)
-  // await db.delete(deviceTable)
-  // await db.delete(userTable)
+  await db.delete(userCredentialTable)
+  await db.delete(deviceTable)
+  await db.delete(userTable)
   for (const user of initUsersData) {
     let userNew = await db.insert(userTable).values(user).returning({ id: userTable.id })
     await db.insert(userCredentialTable).values({
       userId: userNew[0].id,
       passwordHash: user.password,
     })
-    // let users = await db.insert(userTable).values(user).returning({ id: userTable.id })
-    // await db.insert(userCredentialTable).values({
-    //   userId: users[0].id,
-    //   passwordHash: user.password,
-    // })
   }
 
   const userCheck = await db.query.userTable.findFirst({
-    where: eq(userTable.email, "aa@dsd,com"),
+    where: eq(userTable.email, "alan@example.com"),
     columns: { id: true, name: true, image: true },
 
-    // This should drops an error. But why not?
-    with: {
-      userCredential_SHOULD_DROP_A_TYPE_ERROR: {
-        columns: {
-          passwordHash: true,
-        },
-      },
-    },
+    // This should drops an error. But why not? https://github.com/drizzle-team/drizzle-orm/issues/4297
+    // with: {
+    //   userCredential_SHOULD_DROP_A_TYPE_ERROR: {
+    //     columns: {
+    //       passwordHash: true,
+    //     },
+    //   },
+    // },
   })
 
+  console.log(userCheck)
   console.log(`Done!`)
   process.exit(0)
 }
