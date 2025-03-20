0462afdde09837b85316fe49bea0aaa8b128797b
Alan <alan345@gmail.com>
2025-03-20 10:27:06 -0700

    better seed

--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -1,23 +1,47 @@
 import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
-import { userTable, deviceTable, userCredentialTable } from "../db/schema"
+import { drizzleOrm } from "../packageDrizzle"
+const { eq } = drizzleOrm
+import { userTable, userCredentialTable } from "../db/schema"
+// import { userTable, deviceTable, userCredentialTable } from "../db/schema"
+import * as schema from "../db/schema"
 import dotenv from "dotenv"
 dotenv.config({ path: "../../server.env" })
 const databaseUrl = process.env.DATABASE_URL!
+const db = drizzle(databaseUrl, { schema })
 
 const main = async () => {
   console.log(`Seeding ${databaseUrl}...`)
-  const db = drizzle(databaseUrl)
-  await db.delete(userCredentialTable)
-  await db.delete(deviceTable)
-  await db.delete(userTable)
+  // const db = drizzle(databaseUrl)
+  // await db.delete(userCredentialTable)
+  // await db.delete(deviceTable)
+  // await db.delete(userTable)
   for (const user of initUsersData) {
-    let users = await db.insert(userTable).values(user).returning({ id: userTable.id })
+    let userNew = await db.insert(userTable).values(user).returning({ id: userTable.id })
     await db.insert(userCredentialTable).values({
-      userId: users[0].id,
+      userId: userNew[0].id,
       passwordHash: user.password,
     })
+    // let users = await db.insert(userTable).values(user).returning({ id: userTable.id })
+    // await db.insert(userCredentialTable).values({
+    //   userId: users[0].id,
+    //   passwordHash: user.password,
+    // })
   }
+
+  const userCheck = await db.query.userTable.findFirst({
+    where: eq(userTable.email, "aa@dsd,com"),
+    columns: { id: true, name: true, image: true },
+
+    // This should drops an error. But why not?
+    with: {
+      userCredential_SHOULD_DROP_A_TYPE_ERROR: {
+        columns: {
+          passwordHash: true,
+        },
+      },
+    },
+  })
 
   console.log(`Done!`)
   process.exit(0)
