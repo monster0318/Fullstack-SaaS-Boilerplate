6f9b73e12e05db32f0607e02b7ef91e8b8ad7fd7
Alan <alan345@gmail.com>
2025-03-29 22:34:57 -0700

    cleaner

--- client/src/ContextProvider.tsx
+++ client/src/ContextProvider.tsx
@@ -1,6 +1,4 @@
 import React from "react"
-// import { useQuery } from "@tanstack/react-query"
-// import { useTRPC } from "./utils/trpc"
 
 type ContextType = {
   isDarkMode: boolean
@@ -17,35 +15,11 @@ type Props = {
 }
 
 const ContextProvider = (props: Props) => {
-  // const trpc = useTRPC()
-  // const getAuthQuery = useQuery(trpc.getAuth.queryOptions(undefined, { retry: false }))
-
-  // const [isLoadingAuth, setIsLoadingAuth] = React.useState(false)
-  // const [me, setMe] = React.useState<ContextType["me"]>(null)
-  // const [decoded, setDecoded] = React.useState<ContextType["decoded"]>(null)
-  // const [deviceId, setDeviceId] = React.useState<ContextType["deviceId"]>(null)
   const [isDarkMode, setIsDarkMode] = React.useState(false)
 
   const toggleDarkMode = React.useCallback(() => {
     setIsDarkMode((prev) => !prev)
   }, [])
-
-  // const updateAuth = async () => {
-  //   await getAuthQuery.refetch()
-  // }
-
-  // React.useEffect(() => {
-  //   setIsLoadingAuth(getAuthQuery.isLoading)
-  //   if (getAuthQuery.isError) {
-  //     setMe(null)
-  //     return
-  //   }
-  //   if (getAuthQuery.data) {
-  //     setMe(getAuthQuery.data.user)
-  //     setDecoded(getAuthQuery.data.decoded)
-  //     setDeviceId(getAuthQuery.data.deviceid)
-  //   }
-  // }, [getAuthQuery])
 
   return (
     <AppContext.Provider
