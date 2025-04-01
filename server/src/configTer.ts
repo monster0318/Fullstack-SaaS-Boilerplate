bad5676d32164523a5a2c335b59ef3088324856b
Alan <alan345@gmail.com>
2025-03-31 18:35:19 -0700

    beteter files

--- server/src/configTer.ts
+++
@@ -1,4 +0,0 @@
-export const cookieNameAuth = "ter-auth" // this is the cookie name for Auth. You can change it to whatever you want
-export const cookieNameDeviceIds = "ter-device-ids" // this is the cookie name for the device. You can change it to whatever you want
-export const timeSessionCookie = 10 * 60 * 1000 // 10 minutes session in ms
-export const timeDeviceCookie = 400 * 24 * 60 * 60 * 1000 // https://developer.chrome.com/blog/cookie-max-age-expires/ in ms
