9dcaa60c9950b515e495a93c68cd51703f4b3029
Alan <alan345@gmail.com>
2025-04-10 12:45:00 -0700

    Added messageTable deletion in seed process

--- packages/drizzle/src/seed/seed.ts
+++ packages/drizzle/src/seed/seed.ts
@@ -1,7 +1,7 @@
 import { initUsersData } from "./initUsersData"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { eq } from "drizzle-orm"
-import { userTable, verificationTable, accountTable, sessionTable } from "../db/schema"
+import { userTable, verificationTable, accountTable, sessionTable, messageTable } from "../db/schema"
 import * as schema from "../db/schema"
 import dotenv from "dotenv"
 
@@ -16,6 +16,8 @@ const main = async () => {
   await db.delete(accountTable)
   await db.delete(sessionTable)
   await db.delete(userTable)
+  await db.delete(messageTable)
+
   for (const user of initUsersData) {
     let userNew = await db.insert(userTable).values(user).returning({ id: userTable.id })
     await db.insert(accountTable).values({
