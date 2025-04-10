4cbaacd36c8bf26aae87c5fc566c4aa8f21f8fba
Alan <alan345@gmail.com>
2025-04-10 16:41:50 -0700

    better

--- client/src/components/message/SSEConnection.tsx
+++ client/src/components/message/SSEConnection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react"
+import React, { useState, useEffect } from "react"
 import { CircleDot, CircleDotDashed } from "lucide-react"
 import { ChatMessage } from "../../pages/ChatPage"
 
